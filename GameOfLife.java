d0ab5b47290a75d6de16435c4d1e93d20665fd28
asafm47 <asaf.manor@post.runi.ac.il>
2024-02-25 23:03:03 +0200

    fix 2

--- GameOfLife.java
+++ GameOfLife.java
@@ -163,7 +163,7 @@ public class GameOfLife {
 	// Prints the board. Alive and dead cells are printed as 1 and 0, respectively.
     public static void print(int[][] arr) {
 		for(int i= 1 ;i < arr.length-1; i++){
-			for(int j = 1; j-1<arr.length; j++){
+			for(int j = 1; j<arr.length-1; j++){
 				System.out.printf("%d ", arr[i][j]);
 			}
 			System.out.println();
